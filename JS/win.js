51cacf3e47f35f98d9d01b37222201bde1245fad
Thomas <tn900323@gmail.com>
2021-11-03 11:39:18 +0100

    reverted some changes

--- JS/win.js
+++ JS/win.js
@@ -13,10 +13,10 @@ window.addEventListener("load", function () {
 h1.textContent = `${winner} vann!!`;
 resetButton.addEventListener('click', function () {
     localStorage.clear();
-    location.href = "index.html";
+    location.href = "Idiom/index.html";
 })
 replayButton.addEventListener('click', function () {
-    location.href = "game.html";
+    location.href = "Idiom/game.html";
 })
 
 
